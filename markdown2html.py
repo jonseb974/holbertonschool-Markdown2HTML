51bb74b254250ec9e86b21b0292e2de0ededa352
jonseb974 <5151@holbertonstudents.com>
2023-04-04 20:14:16 +0200

    add new script test

--- markdown2html.py
+++ markdown2html.py
@@ -3,32 +3,22 @@
     First argument is the name of the Markdown file
     Second argument is the output file name
 """
-
 import sys
 import os
-import markdown
 
 
-if __name__ == "__main__":
-
-    if len(sys.argv) < 3:
-        print("Usage: {} <markdown_file> <output_file>".format(sys.argv[0]),
-    file=sys.stderr)
-        sys.exit(1)
-
-markdown_file = sys.argv[1]
-output_file = sys.argv[2]
-
-if not os.path.exists(markdown_file):
-    print("Missing {}".format(markdown_file), file=sys.stderr)
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py <inputfile.md> <outputfile.html>",
+        file=sys.stderr)
     sys.exit(1)
 
-with open(markdown_file, 'r') as md_file:
-    markdown_text = md_file.read()
+input_file = sys.argv[1]
+output_file = sys.argv[2]
 
-html_text = markdown.markdown(markdown_text)
+if not os.path.isfile(input_file):
+    print(f"Missing {input_file}", file=sys.stderr)
+    sys.exit(1)
 
-with open(output_file, 'w') as out_file:
-    out_file.write(html_text)
+# TODO: Convert the Markdown file to HTML and write to the output file
 
 sys.exit(0)
