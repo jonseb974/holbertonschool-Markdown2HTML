cb7e493683fbecf3e1438a8c37d64074d35a8882
jonseb974 <5151@holbertonstudents.com>
2023-04-04 17:28:15 +0200

    add new markdown2html_file

--- markdown2html.py
+++ markdown2html.py
@@ -1,35 +1,26 @@
-#!/usr/bin/python3
-"""
-task 0 start a script
-"""
+#!/usr/bin/env python3
+import markdown
+import sys
+import os
 
+if len(sys.argv) < 3:
+    print("Usage: {} <markdown_file> <output_file>".format(sys.argv[0]), file=sys.stderr)
+    sys.exit(1)
 
-import sys  # import the sys module
-import os  # import os module
+markdown_file = sys.argv[1]
+output_file = sys.argv[2]
 
+if not os.path.exists(markdown_file):
+    print("Missing {}".format(markdown_file), file=sys.stderr)
+    sys.exit(1)
 
-def convert_markdown_to_html(self, markdown_file, html_file):
-    """
-    Convert markdown to html
-    """
-    self.markdown_file = markdown_file
-    self.html_file = html_file
-    pass
+with open(markdown_file, 'r') as md_file:
+    markdown_text = md_file.read()
 
+html_text = markdown.markdown(markdown_text)
 
-# Check if the script is executed directly(not imported)
-if __name__ == "main":
+with open(output_file, 'w') as out_file:
+    out_file.write(html_text)
 
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", \
-              file=sys.stderr)
-        sys.exit(1)
-    markdown_file = sys.argv[1]  # Get the filename
-    html_file = sys.argv[2]  # Output name
-
-if not os.path.isfile(markdown_file):  # Check if file exists.
-    print(f"Missing {markdown_file}", file=sys.stderr)  # error message
-    sys.exit(1)  # exit with non-zero status code
-convert_markdown_to_html(markdown_file, html_file)
 sys.exit(0)
-# exit with zero-status code if success
+
