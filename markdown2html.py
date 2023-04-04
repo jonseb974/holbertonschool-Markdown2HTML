59fbd592689149545f478a836b1834a8a77d1a1c
jonseb974 <5151@holbertonstudents.com>
2023-04-04 22:22:19 +0200

    update

--- markdown2html.py
+++ markdown2html.py
@@ -10,21 +10,30 @@ import os  # import os module
 
 
 def convert_markdown_to_html(input_file, output_file):
+    # TODO: Implement Markdown to HTML conversion here
     pass
 
 
-# Check if the script is executed directly (not imported)
-if __name__ == "main":
+if __name__ == "__main__":
+    # Check if the number of command-line arguments is correct
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py READMe.md README.html",
-        file=sys.stderr)
+        # Print an error message to STDERR and exit with a non-zero status code
+        print(
+            "Usage: ./markdown2html.py README.md README.html",
+            file=sys.stderr)
         sys.exit(1)
-    input_file = sys.argv[1]  # Get the filename
-    output_file = sys.argv[2]  # Output name
 
-    if not os.path.isfile(input_file):  # Check if file exists.
-        print(f"Missing {input_file}", file=sys.stderr)  # error message
-        sys.exit(1)  # exit with non-zero status code
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-    convert_markdown_to_html(input_file, output_file)  # Convert to html
-    sys.exit(0)  # exit with zero-status code if success
+    # Check if the input file exists
+    if not os.path.isfile(input_file):
+        # Print an error message to STDERR and exit with a non-zero status code
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # Convert Markdown to HTML and write to output_file
+    convert_markdown_to_html(input_file, output_file)
+
+    # Exit with a zero status code to indicate success
+    sys.exit(0)
