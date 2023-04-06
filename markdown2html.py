08e3e87f0f50a4e02b958bf11428665630346620
jonseb974 <5151@holbertonstudents.com>
2023-04-06 12:30:41 +0200

    new script test

--- markdown2html.py
+++ markdown2html.py
@@ -1,66 +1,69 @@
 #!/usr/bin/python3
-""" task_0 Write a script markdown2html.py that takes an argument 2 strings:
-    First argument is the name of the Markdown file Second argument is
-    the output file name :README.md README.html
-    Improve markdown2html, parsing Headings Markdown syntax for generating HTML
-"""
+"""Convert Markdown file to HTML"""
 
-import sys  # import the sys module
-import os  # import os module
+import argparse
+import os
+import sys
+import re
 
 
-
-def convert_markdown_to_html(input_file, output_file):
-    # open read input file, read all lines
-    with open(input_file, "r") as f:
-        lines = f.readlines()
-
-    # open write output file
-    with open(output_file, "w") as f:
-        # Foe each line of the file
-        for line in lines:
-            if line.startswith("#"):  # check if line starts with 1 # or more
-                heading_level = 1  # item of level 1
-                while line.startswith("#", heading_level):
-                    heading_level += 1
-                heading_text = line[heading_level:].strip()
-                f.write(
-                    f'<h{heading_level}>{heading_text}</h{heading_level}>\n'
-                    )
-            elif line.startswith("-"):  # check if line starts with '-'
-                f.write("<ul>\n")  # starts a new unordered list
-                while line.startswith("-"):
-                    line = line[1:].strip()  # remove "-" an white spaces
-                    f.write(f"<li>{line}</li>\n")  # add line as list
-                    try:
-                        line = lines.pop(0)  # get the next line
-                    except IndexError:
-                        break  # end of file
-                f.write("</ul>\n")  # end of the unordered list
-            else:  # If line do not start with #, write it in output
-                f.write(line)
+def markdown_to_html(input_file, output_file):
+    """Converts a Markdown file to HTML"""
+    try:
+        with open(
+            input_file, "r"
+                  ) as md_file, open(output_file, "w") as html_file:
+            md_text = md_file.read()
+            # convert headers
+            md_text = re.sub(r'###### (.+)', r'<h6>\1</h6>', md_text)
+            md_text = re.sub(r'##### (.+)', r'<h5>\1</h5>', md_text)
+            md_text = re.sub(r'#### (.+)', r'<h4>\1</h4>', md_text)
+            md_text = re.sub(r'### (.+)', r'<h3>\1</h3>', md_text)
+            md_text = re.sub(r'## (.+)', r'<h2>\1</h2>', md_text)
+            md_text = re.sub(r'# (.+)', r'<h1>\1</h1>', md_text)
+            # convert bold and italic
+            md_text = re.sub(r'\*\*(.+?)\*\*', r'<strong>\1</strong>', md_text)
+            md_text = re.sub(r'__(.+?)__', r'<strong>\1</strong>', md_text)
+            md_text = re.sub(r'\*(.+?)\*', r'<em>\1</em>', md_text)
+            md_text = re.sub(r'_(.+?)_', r'<em>\1</em>', md_text)
+            # convert lists
+            md_text = re.sub(r'^\* (.+)', r'<li>\1</li>', md_text, flags=re.M)
+            md_text = re.sub(
+                r'^(\* .+\n)+', r'<ul>\n\g<0></ul>', md_text, flags=re.M
+                            )
+            md_text = re.sub(
+                r'^[0-9]+\. (.+)', r'<li>\1</li>', md_text, flags=re.M
+                            )
+            md_text = re.sub(
+                r'^([0-9]+\. .+\n)+', r'<ol>\n\g<0></ol>', md_text, flags=re.M
+                            )
+            # convert links
+            md_text = re.sub(
+                r'\[([^\]]+)\]\(([^\)]+)\)', r'<a href="\2">\1</a>', md_text
+                            )
+            # convert paragraphs
+            md_text = re.sub(
+                r'^([^\n].+\n)+', r'<p>\g<0></p>', md_text, flags=re.M
+                            )
+            # write to file
+            html_file.write(md_text)
+    except Exception as e:
+        print(f"Error converting {input_file} to {output_file}: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    # Check if the number of command-line arguments is correct
-    if len(sys.argv) < 3:
-        # Print an error message to STDERR and exit with a non-zero status code
-        print(
-            "Usage: ./markdown2html.py README.md README.html",
-            file=sys.stderr)
+    parser = argparse.ArgumentParser(
+        description="Convert Markdown file to HTML"
+                                    )
+    parser.add_argument("input_file", help="The input Markdown file")
+    parser.add_argument("output_file", help="The output HTML file")
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.input_file):
+        print(f"Error: {args.input_file} does not exist")
         sys.exit(1)
 
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+    markdown_to_html(args.input_file, args.output_file)
 
-    # Check if the input file exists
-    if not os.path.isfile(input_file):
-        # Print an error message to STDERR and exit with a non-zero status code
-        print(f"Missing {input_file}", file=sys.stderr)
-        sys.exit(1)
-
-    # Convert Markdown to HTML and write to output_file
-    convert_markdown_to_html(input_file, output_file)
-
-    # Exit with a zero status code to indicate success
     sys.exit(0)
