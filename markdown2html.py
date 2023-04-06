06c43b730d59ac3a3808749698f47aeb42b97221
jonseb974 <5151@holbertonstudents.com>
2023-04-06 15:27:30 +0200

    add file scripte

--- markdown2html.py
+++ markdown2html.py
@@ -3,36 +3,42 @@
     First argument is the name of the Markdown file
     Second argument is the output file name
 '''
-import os
 import sys
+import os
 
 
 def convert_markdown_to_html(input_file, output_file):
-    # open read input file, read all lines
-    with open(input_file, "r") as f:
+    with open(input_file, 'r') as f:
         lines = f.readlines()
 
-    # open write output file
-    with open(output_file, "w") as f:
-        # For each line of the file
+    with open(output_file, 'w') as f:
         for line in lines:
-            if line.startswith("#"):  # check if line starts with 1 # or more
-                heading_level = 1  # item of level 1
-                while line.startswith("#", heading_level):
+            if line.startswith('# '):
+                heading_level = 1
+                while line.startswith('#', heading_level):
                     heading_level += 1
                 heading_text = line[heading_level:].strip()
                 f.write(
                     f'<h{heading_level}>{heading_text}</h{heading_level}>\n'
-                    )
-            elif line.startswith("-"):  # check if line starts with '-'
-                f.write("<ul>\n")  # start a new unordered list
-                while line.startswith("-"):
-                    line = line[1:].strip()  # remove '-' and whitespaces
-                    f.write(f"<li>{line}</li>\n")  # add line as a list item
-                    try:
-                        line = lines.pop(0)  # get the next line
-                    except IndexError:
-                        break  # end of file
-                f.write("</ul>\n")  # end the unordered list
-            else:  # If line do not start with # or -, write it in output
+                       )
+            else:
                 f.write(line)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(
+            "Usage: ./markdown2html.py README.md README.html", file=sys.stderr
+             )
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.isfile(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
+
+    convert_markdown_to_html(input_file, output_file)
+
+    sys.exit(0)
